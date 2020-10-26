f5274f2e7918e05cc0cbdcd7e39c75b09e46d71a
morann <ania.morys@gmail.com>
2020-10-26 17:38:20 +0100

    Climbing shop welcome

---
+++ src/main/java/pl/sda/climbing_shop/ClimbingShopApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.climbing_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/climbing_shop/ClimbingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.climbing_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
