d472c9c762d294feb7ffe502f272a8231c41baa4
morann <ania.morys@gmail.com>
2020-11-15 10:08:51 +0100

    - extending CategoryRepository and ProductRepository by findCategoryByCategoryName and findProductsByCategory_CategoryName - refactoring CategoryRepository

--- src/main/java/pl/sda/climbing_shop/controller/ClimbingGearController.java
+++ src/main/java/pl/sda/climbing_shop/controller/ClimbingGearController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.climbing_shop.category.Category;
 import pl.sda.climbing_shop.category.CategoryRepository;
 import pl.sda.climbing_shop.product.Product;
@@ -14,54 +15,24 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/climbingGear")
 public class ClimbingGearController {
 
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
 
-    @GetMapping("/climbingGear")
+    @GetMapping
     public String viewClimbingGear() {
         return "climbingGear";
     }
 
-    @GetMapping("/climbingGear/{id}")
-    public String viewClimbingShoes(@PathVariable("id") int id, Model model) {
-        Category category = this.categoryRepository.findById(id).get();
+    @GetMapping("/{categoryName}")
+    public String viewClimbingGearCategory(@PathVariable("categoryName") String name, Model model) {
+        Category category = this.categoryRepository.findCategoryByCategoryName(name).get();
         model.addAttribute("category", category);
-        System.out.println(category.getCategoryName());
-        List<Product> products = this.productRepository.findProductsByCategory_CategoryId(id);
+        List<Product> products = this.productRepository.findProductsByCategory_CategoryName(name);
         model.addAttribute("products", products);
-        return "climbingShoes";
-    }
-
-    @GetMapping("/climbingGear/harnesses")
-    public String viewHarnesses() {
-        return "harnesses";
-    }
-
-    @GetMapping("/climbingGear/belay_rappel")
-    public String viewBelayRappel() {
-        return "belayRappel";
-    }
-
-    @GetMapping("/climbingGear/carabiners")
-    public String viewCarabiners() {
-        return "carabiners";
-    }
-
-    @GetMapping("/climbingGear/quickdraws")
-    public String viewQuickdraws() {
-        return "quickdraws";
-    }
-
-    @GetMapping("/climbingGear/ropes")
-    public String viewRopes() {
-        return "ropes";
-    }
-
-    @GetMapping("/climbingGear/helmets")
-    public String viewHelmets() {
-        return "helmets";
+        return name;
     }
 
 }
